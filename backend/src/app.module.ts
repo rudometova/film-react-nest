bb75f0f3980e77254f6dddea8847221dda41a3eb
Valentina Rudometova <valentina@rudometova.ru>
2026-05-12 12:28:56 +0300

    Скорректированы переменные окружения

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { configProvider } from './app.config.provider';
 import { FilmsModule } from './films/films.module';
 import { OrderModule } from './order/order.module';
@@ -11,23 +12,23 @@ import { OrderModule } from './order/order.module';
       isGlobal: true,
       cache: true,
     }),
-
+    
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        host: 'localhost',
-        port: 5432,
+        type: configService.get<string>('DATABASE_DRIVER') as 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
         username: configService.get<string>('DATABASE_USERNAME'),
         password: configService.get<string>('DATABASE_PASSWORD'),
-        database: 'prac',
+        database: configService.get<string>('DATABASE_NAME'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: false,
         logging: true,
       }),
       inject: [ConfigService],
     }),
-
+    
     FilmsModule,
     OrderModule,
   ],
